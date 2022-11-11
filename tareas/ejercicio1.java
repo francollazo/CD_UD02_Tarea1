8280d9dc985c9b3236b0999827580e32c6a055d6
francollazo <114486733+francollazo@users.noreply.github.com>
2022-11-11 22:17:27 +0100

    Commit ejercicio1

--- tareas/ejercicio1.java
+++ tareas/ejercicio1.java
@@ -6,7 +6,6 @@ Para conocer el signo del horóscopo chino, debemos
 dividir el año de nacimiento entre 12. El resto, entre
 0 y 11, está asociado a un signo según la siguiente
 tabla:
-
 0 -Mono
 1- Gallo
 2 -Perro
@@ -19,5 +18,31 @@ tabla:
 9 -Serepiente
 10- Caballo
 11 - Cabra 
-
  */
+
+ import java.util.Scanner;
+
+ public class ejercicio1 {
+
+    public static void main(String[] args) {
+        System.out.println("Cual es tu fecha de nacimiento?");
+        Scanner sc = new Scanner(System.in);
+        int fecha = sc.nextInt();
+        int numero = fecha % 12;
+        switch (numero) {
+            case 0: horoscopo = "Mono"; break;
+            case 1: horoscopo = "Gallo"; break;
+            case 2: horoscopo = "Perro"; break;
+            case 3: horoscopo = "Cerdo"; break;
+            case 4: horoscopo = "Rata"; break;
+            case 5: horoscopo = "Buey"; break;
+            case 6: horoscopo = "Tigre"; break;
+            case 7: horoscopo = "Conejo"; break;
+            case 8: horoscopo = "Dragón"; break;
+            case 9: horoscopo = "Serpiente"; break;
+            case 10: horoscopo = "Caballo"; break;
+            case 11: horoscopo = "Cabra"; break;   
+        }
+        System.out.println("Signo del zodiaco: " +horoscopo);
+    }
+ }
